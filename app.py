3f0c9c0ffa9e51ad5591db7a756ec2dff7716afd
LimDong <50188317+Limdongdang@users.noreply.github.com>
2024-05-19 04:04:36 +0900

    모델 변경

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from gensim.models import Word2Vec
+from gensim.models import FastText
 import pandas as pd
 
 # 데이터 전처리
@@ -8,7 +8,36 @@ df = pd.read_excel('projects.xlsx')
 projects = df['Keyword'].str.split(',')
 
 # Word2Vec 모델 학습
-model = Word2Vec(projects, min_count=1, sg=1, negative=5)
+# model = Word2Vec(projects, min_count=1, sg=1, negative=5)
+# FastText 모델 학습
+model = FastText(sentences=projects, min_count=1, sg=1, negative=5, epochs=10, hs=0, vector_size=100, window=9999999, workers=4)
+
+
+def recommend_project(input_keywords):
+    # 모든 프로젝트에 대한 유사도 점수를 저장할 딕셔너리
+    project_scores = {}
+
+    # 모든 프로젝트를 순회
+    for i, project in enumerate(projects):
+        # 프로젝트의 키워드와 입력 키워드 간의 유사도를 저장할 리스트
+        similarities = []
+
+        # 프로젝트의 모든 키워드를 순회
+        for keyword in project:
+            # 키워드가 모델의 단어 벡터에 존재하는 경우에만 유사도를 계산
+            for input_keyword in input_keywords:
+                if keyword in model.wv and input_keyword in model.wv:
+                    similarity = model.wv.similarity(keyword, input_keyword)
+                    similarities.append(similarity)
+
+        # 프로젝트의 유사도 점수는 키워드 간 유사도의 평균
+        if similarities:
+            project_scores[i] = sum(similarities) / len(similarities)
+
+    # 가장 높은 점수를 가진 프로젝트를 찾음
+    most_similar_project_index = max(project_scores, key=project_scores.get)
+
+    return projects[most_similar_project_index]
 
 def calculate_similarity_scores(tech_stack):
     # 모든 프로젝트에 대한 유사도 점수를 저장할 딕셔너리
@@ -42,6 +71,8 @@ def get_most_similar_project(tech_stack):
     return most_similar_project
 
 # 예시
-print(calculate_similarity_scores(['Phone']))
+print(calculate_similarity_scores(['소프트웨어']))
 # 예시
-print(get_most_similar_project(['Phone']))
+print(get_most_similar_project(['소프트웨어', '모바일', '텔레콤', 'Phone', '쇼핑몰']))
+
+print(recommend_project(['모바일앱', '텔레콤']))
